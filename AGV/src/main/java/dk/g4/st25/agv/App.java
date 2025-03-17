f2516b263c15dc18d4b083d37f39531dbf83ca78
silasjul <silaskierstein@gmail.com>
2025-03-17 13:41:30 +0100

    Component setup is done, and ready for more

---
+++ AGV/src/main/java/dk/g4/st25/agv/App.java
@@ -0,0 +1,13 @@
+package dk.g4.st25.agv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

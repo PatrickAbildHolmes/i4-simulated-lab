b64d4c2811c0399b33753cbef89684d3ce360fe9
Anders-Hedegaard <anhed23@student.sdu.dk>
2025-03-21 12:13:18 +0100

    Created the structure for the MQTT protocol

---
+++ Protocols/src/main/java/dk/g4/st25/protocols/MQTT.java
@@ -0,0 +1,17 @@
+package dk.g4.st25.protocols;
+
+import org.eclipse.paho.client.mqttv3.*;
+
+public class MQTT {
+
+    public MqttClient connectToBroker() {
+
+        return null;
+    }
+    public void subscribeToTopic(MqttClient client, String topic) {
+
+    }
+    public void publishMessage(MqttClient client, String topic, String message) {
+
+    }
+}

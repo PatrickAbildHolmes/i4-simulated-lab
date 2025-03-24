a93f55c61ef0444461703100ce68ec472929a6ee
Nikolai Bruun <nibru23@student.sdu.dk>
2025-03-24 14:46:37 +0100

    Removed redundant calls for switching between stages. Created SceneController. Moved FXML files to new directory.
    I have removed the redundant calls for the same action in the homepage controller, and added a switchToHomepage in the new SceneController class. I have also moved the FXML files, as i think they were in the wrong directory, if we needed to fetch them. They might still be hard to retrieve, as in the SceneController, there is a "maybe" null-called function.

---
+++ Core/src/main/java/dk/g4/st25/core/uicontrollers/SceneController.java
@@ -0,0 +1,27 @@
+package dk.g4.st25.core.uicontrollers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+public class SceneController {
+    // Switching between scenes. This takes an FXML file as a parameter
+    public void switchScene(ActionEvent event, String fxmlFile) throws IOException {
+        System.out.println(fxmlFile);
+        Parent root = FXMLLoader.load((getClass().getResource(fxmlFile)));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToHomepage(ActionEvent event) throws IOException {
+        //switchScene(event, "homepage.fxml");
+    }
+}

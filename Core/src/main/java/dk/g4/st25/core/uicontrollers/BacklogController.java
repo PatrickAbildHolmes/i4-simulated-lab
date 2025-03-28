a2a6a3fa8e375d001c3e61f27bce11b3c65e1f24
Nikolai Bruun <nibru23@student.sdu.dk>
2025-03-28 11:33:53 +0100

    Made switchToHomepage static and called in every controller
    The switchToHomepage controller was redundant, as it was the same function called everywhere. Therefore, a static method was made in the sceneController, which can be called everywhere

--- Core/src/main/java/dk/g4/st25/core/uicontrollers/BacklogController.java
+++ Core/src/main/java/dk/g4/st25/core/uicontrollers/BacklogController.java
@@ -114,11 +114,7 @@ public class BacklogController {
 
     // Method for switching back to the "Homepage" site
     public void switchToHomepage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        SceneController.switchToHomepage(event);
     }
 
     // Initializes all functionalities when the scene is opened
--- Core/src/main/java/dk/g4/st25/core/uicontrollers/SetParametersController.java
+++ Core/src/main/java/dk/g4/st25/core/uicontrollers/SetParametersController.java
@@ -30,12 +30,9 @@ public class SetParametersController {
 
     // Method for switching back to the "Homepage" site
     public void switchToHomepage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        SceneController.switchToHomepage(event);
     }
+
     // Initializes all functionalities when the scene is opened
     public void initialize(){
         // Applies hovering effect to increase size

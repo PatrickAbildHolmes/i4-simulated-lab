d0588554447eb2c65d41140ec955c14acdb868f6
Nikolai Bruun <nibru23@student.sdu.dk>
2025-04-15 10:35:24 +0200

    Added blade files

--- DroneAssembly/SOAP/src/main/java/dk/g4/st25/soap/SOAPtestforsoeg.java
+++ DroneAssembly/SOAP/src/main/java/dk/g4/st25/soap/SOAPtestforsoeg.java
@@ -3,13 +3,15 @@ package dk.g4.st25.soap;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 
-import javax.swing.text.Document;
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.ByteArrayInputStream;
 
+import kong.unirest.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
 public class SOAPtestforsoeg {
-    public void getInventory() {
+    public JSONObject getInventory() {
         try {
             HttpResponse<String> response = Unirest.post("http://localhost:8081/Service.asmx")
                     .header("Content-Type", "text/xml; charset=utf-8")
@@ -20,19 +22,44 @@ public class SOAPtestforsoeg {
                             "</soap:Body>\n</soap:Envelope>\n")
                     .asString();
 
+            System.out.println("HTTP Status: " + response.getStatus());  // Check status code
+            System.out.println("SOAP Response: " + response.getBody()); // Print full response
             if (response.getStatus() == 200) {
-                String soapresponse = response.getBody();
+                String soapResponse = response.getBody();
+
                 // We have to parse the XML in an XML DOM
-                Document doc = (Document) DocumentBuilderFactory.newInstance()
+                // This simplifies the XML file and makes it able to list the XML file much easier
+                Document doc = DocumentBuilderFactory.newInstance()
                         .newDocumentBuilder()
-                        .parse(new ByteArrayInputStream(soapresponse.getBytes()));
+                        .parse(new ByteArrayInputStream(soapResponse.getBytes()));
+                doc.getDocumentElement().normalize();
 
 
+                // We fetch the first place in the XML file that reads getInventory and lists the json object from that
+                NodeList nl = doc.getElementsByTagName("GetInventoryResult");
+                if (nl.getLength() > 0) {
+                    String jsonString = nl.item(0).getTextContent();
+                    // Return the parsed JSON
+                    return new JSONObject(jsonString);
+
+                } else {
+                    // Error handling if getInventory is not found
+                    JSONObject errorJson = new JSONObject();
+                    errorJson.put("error", "GetInventoryResult element not found");
+                    return errorJson;
+                }
             } else {
-                System.out.println(response.getStatusText());
-            }
+                    // Error handling if the getStatus does not equal 200
+                    JSONObject errorJson = new JSONObject();
+                    errorJson.put("error", response.getStatusText());
+                    return errorJson;
+                }
         } catch (Exception e) {
-            e.printStackTrace();
+            // catch block for the try-catch
+            // Needed for handling JSON object
+            JSONObject errorJson = new JSONObject();
+            errorJson.put("exception", e.getMessage());
+            return errorJson;
         }
     }
 
@@ -78,8 +105,8 @@ public class SOAPtestforsoeg {
 
     public static void main(String[] args) {
         SOAPtestforsoeg soaptestforsoeg = new SOAPtestforsoeg();
-        soaptestforsoeg.getInventory();
-        soaptestforsoeg.pickItem();
-        soaptestforsoeg.insertItem();
+        System.out.println(soaptestforsoeg.getInventory());
+        //soaptestforsoeg.pickItem();
+        //soaptestforsoeg.insertItem();
     }
 }
